ed9cef0f39340c654a802d7122ec35f5f029fcd2
Samyukta Twarakavi <samyukta@Samyuktas-Laptop.local>
2025-10-07 18:57:44 +0800

    codes for various robots

---
+++ For_class/Clawbot/Basic_claw_bot_program/src/main.cpp
@@ -0,0 +1,97 @@
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+
+#include "vex.h"
+
+using namespace vex;
+
+// Brain should be defined by default
+brain Brain;
+
+
+// START V5 MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+
+// Robot configuration code.
+motor LeftDriveSmart = motor(PORT10, ratio18_1, true);
+motor RightDriveSmart = motor(PORT1, ratio18_1, false);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+
+motor Arm_Joint = motor(PORT8, ratio18_1, false);
+
+motor Claw = motor(PORT3, ratio18_1, false);
+
+
+
+// generating and setting random seed
+void initializeRandomSeed(){
+  int systemTime = Brain.Timer.systemHighResolution();
+  double batteryCurrent = Brain.Battery.current();
+  double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
+
+  // Combine these values into a single integer
+  int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
+
+  // Set the seed
+  srand(seed);
+}
+
+
+
+void vexcodeInit() {
+
+  //Initializing random seed.
+  initializeRandomSeed(); 
+}
+
+
+// Helper to make playing sounds from the V5 in VEXcode easier and
+// keeps the code cleaner by making it clear what is happening.
+void playVexcodeSound(const char *soundName) {
+  printf("VEXPlaySound:%s\n", soundName);
+  wait(5, msec);
+}
+
+#pragma endregion VEXcode Generated Robot Configuration
+
+// Include the V5 Library
+#include "vex.h"
+  
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+float myVariable;
+
+// "when started" hat block
+int whenStarted1() {
+  return 0;
+}
+
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  whenStarted1();
+  // Code for navigating the labrynth
+   Drivetrain.driveFor(forward, 900, mm);
+  Drivetrain.turnFor(right, 85, degrees);
+  Drivetrain.driveFor(forward, 1810, mm);
+  Drivetrain.turnFor(left, 85, degrees);
+  Drivetrain.driveFor(forward, 500, mm);
+  Drivetrain.turnFor(left, 85, degrees);
+  Drivetrain.driveFor(forward, 520, mm);
+}
